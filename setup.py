bc9ee701c209ed0d26e224af962e7146c7a5608e
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-07-21 19:12:17 +0200

    Updated setup file and created requirements file

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import numpy
 from setuptools import setup
 
+with open("requirements.txt") as requires_file:
+    requirements = requires_file.read().split("\n")
+
 setup(
-    name = 'antsampler',
-    description = 'AntSampler',
-    author = 'Stefano Rinaldi, Daniele Sanfratello',
-    author_email = 'stefano.rinaldi@phd.unipi.it, d.sanfratello@studenti.unipi.it',
-    url = 'https://github.com/sterinaldi/ant-sampler',
-    python_requires = '>=3.7',
-    packages = ['antsampler'],
-    include_dirs = [numpy.get_include()],
+    name='antsampler',
+    description='AntSampler',
+    author='Stefano Rinaldi, Daniele Sanfratello',
+    author_email='stefano.rinaldi@phd.unipi.it, d.sanfratello@studenti.unipi.it',
+    url='https://github.com/sterinaldi/ant-sampler',
+    python_requires='>=3.7',
+    packages=['antsampler'],
+    install_requires=requirements,
+    include_dirs=[numpy.get_include()],
     setup_requires=['numpy']
     )
 
